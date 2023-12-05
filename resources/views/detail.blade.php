6e00b8727e629618bb84fb7db2443c164805cfbe
Ni Made Ayu Wira Salindri <110331363+ayuwirasalindri@users.noreply.github.com>
2023-12-05 16:38:43 +0700

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,23 +5,20 @@
     <div class="container mt-4">
         <div class="row">
             <div class="col-lg-6">
-                {{-- <p>{{ $product->id}}</p> --}}
-            </div>
-
-            {{-- <div class="col-lg-6">
-                <img src="{{ $product->picture }}" class="rounded mb-1" style="width: 450px" />
-            </div> --}}
-            <div class="col-lg-6">
+                <img src="{{ asset('storage/' . $product->picture) }}" class="rounded mb-1" style="width: 200px" />
                 <div class="card-body mt-2">
                     <p class="card-title card2">
                         <b>{{ $product->name }}</b>
                     </p>
-                    <!-- button -->
-                    <p class="card2 price">{{ $product->price }}</p>
-                    <p class="card4 mt-1">Product Details</p>
                     <p class="card1">
                         {{ $product->description }}
                     </p>
+                    <p class="card2 price">{{ $product->price }}</p>
+                </div>
+                <div class="mt-4 mb-5">
+                    <a href="/products" class="btn btn-dark" style="background-color: #A21608;">
+                        Back
+                    </a>
                 </div>
             </div>
         </div>
