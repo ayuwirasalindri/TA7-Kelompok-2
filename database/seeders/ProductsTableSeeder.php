a582b4cc00e0e8ca9727aef0c202cd7e88497bce
Ni Made Ayu Wira Salindri <110331363+ayuwirasalindri@users.noreply.github.com>
2023-12-05 16:36:59 +0700

    Update ProductsTableSeeder.php

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Product;
 use Carbon\Carbon;
 
@@ -17,12 +18,40 @@ class ProductsTableSeeder extends Seeder
         $product = [
             [
                 'id' => '1',
-                'name' => 'Airpods',
-                'description' => 'A perfect balance of high-fidelity',
+                'picture' => 'uploads/hoodie.jpg',
+                'name' => 'Hoodie',
+                'description' => 'A perfect balance of ',
                 'price' => 4000000,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
+            [
+                'id' => '2',
+                'picture' => 'uploads/bloetooth.jpg',
+                'name' => "Bloetooth",
+                'description' => 'A perfect balance of bloetooth',
+                'price' => 3000000,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'id' => '3',
+                'picture' => 'uploads/sneakers.jpg',
+                'name' => 'Sneakers',
+                'description' => 'A perfect balance of shoes',
+                'price' => 2300000,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'id' => '4',
+                'picture' => 'uploads/desk.jpg',
+                'name' => 'Desk',
+                'description' => 'A perfect balance of desk',
+                'price' => 4500000,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
         ];
         Product::truncate();
         Product::insert($product);
