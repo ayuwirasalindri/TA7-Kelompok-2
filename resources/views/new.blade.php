dea78bcb167e7bbc0db8b9baa99a572baec4d1ce
Ni Made Ayu Wira Salindri <110331363+ayuwirasalindri@users.noreply.github.com>
2023-12-05 16:39:28 +0700

    Update new.blade.php

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -2,7 +2,7 @@
 @section('page_title', 'TelUFinds')
 @section('content')
 
-    <h3>Tambah Product</h3>
+    <h3>Add Product</h3>
         <div class="row">
             <form action="/product" method="post">
                 @csrf
@@ -14,7 +14,13 @@
                     </div>
                 </div>
                 <div class="form-group row mt-4">
-                    <label for="name" class="col-2 col-form-label">Detail Product</label>
+                    <label for="picture" class="col-2 col-form-label">Product's Picture</label>
+                    <div class="col-sm-5">
+                        <input type="file" class="form-control-file" id="picture" name="picture">
+                    </div>
+                </div>
+                <div class="form-group row mt-4">
+                    <label for="name" class="col-2 col-form-label">Description</label>
                     <div class="col-sm-5">
                         <input type="text" class="form-control" id="description" name="description">
                     </div>
@@ -26,11 +32,10 @@
                     </div>
                 </div>
                 <div class="mt-4 mb-5">
-                    <button type="submit" class="btn btn-outline-secondary">Perbarui</button>
                     <a href="/products" class="btn btn-dark" style="background-color: #A21608;">
-                        Kembali
+                        Back
                     </a>
-                    
+                    <button type="submit" class="btn btn-outline-primary">Add</button>
                 </div>
             </form>
         </div>
