a78cc281d9ac13a747be00d0c0520f2a44fbe6ed
bitaridmr <149652967+bitaridmr@users.noreply.github.com>
2024-01-03 18:33:06 +0700

    Update productscontroller.php

--- app/Http/Controllers/productscontroller.php
+++ app/Http/Controllers/productscontroller.php
@@ -1,15 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Http\Requests\CategoryCreateRequest;
+use Illuminate\Support\Facades\Storage;
 
-class productscontroller extends Controller
-{    public function index(Request $request)
+class ProductsController extends Controller
+{
+    public function home(Request $request)
     {
-        $products = Product::orderBy('id')->simplePaginate(6);
+        $products = Product::where('name', 'LIKE', '%' . $request->keyword . '%')->orderBy('name')->simplePaginate(8);
+
+        return view('home.home', ['products' => $products]);
+    }
+
+    public function homelogin(Request $request)
+    {
+        $products = Product::where('name', 'LIKE', '%' . $request->keyword . '%')->orderBy('name')->simplePaginate(8);
+
+        return view('home.homelogin', ['products' => $products]);
+    }
+    public function index(Request $request)
+    {
+        $products = Product::where('name', 'LIKE', '%' . $request->keyword . '%')->orderBy('name')->simplePaginate(8);
 
         return view('product.index', ['products' => $products]);
     }
@@ -22,12 +40,37 @@ class productscontroller extends Controller
 
     public function update(Request $request, $id)
     {
-        $product = Product::findOrFail($id);
-        $product->update($request->all());
+        try {
+            $product = Product::findOrFail($id);
 
-        if ($product) {
+            $request->validate([
+                'name' => 'required|string',
+                'description' => 'required|string',
+                'price' => 'required|integer',
+                'picture' => 'image|mimes:jpeg,png,jpg,gif',
+            ]);
+
+            $productData = $request->all();
+
+            // Proses upload gambar jika ada
+            if ($request->hasFile('picture')) {
+                Storage::delete('public/uploads/' . $product->picture);
+                $imageName = time() . '.' . $request->picture->getClientOriginalExtension();
+                $request->picture->storeAs('public/uploads/', $imageName);
+                $productData['picture'] = $imageName;
+            }
+            $product->update($productData);
+
             Session::flash('status', 'success');
             Session::flash('message', 'Perubahan data berhasil');
+        } catch (\Illuminate\Database\QueryException $e) {
+            // Menangani kesalahan SQL, misalnya duplikasi data
+            Session::flash('status', 'error');
+            Session::flash('message', 'Perubahan data gagal: ' . $e->getMessage());
+        } catch (\Exception $e) {
+            // Menangani kesalahan umum
+            Session::flash('status', 'error');
+            Session::flash('message', 'Perubahan data gagal: ' . $e->getMessage());
         }
 
         return redirect('/products');
@@ -35,44 +78,57 @@ class productscontroller extends Controller
 
     public function detail($name)
     {
-        $product = DB::table('products')->where('name', $name)->first();
+        $product = Product::where('name', $name)->firstOrFail();
 
         return view('product.detail', ['product' => $product]);
     }
 
+
     public function new()
     {
-        return view('product.new');
+        $categories = Category::all(); // Ambil semua data kategori dari tabel categories
+        return view('product.new', ['categories' => $categories]);
     }
 
     public function store(Request $request)
     {
-        $product = product::create($request->all());
+        try {
+            $request->validate([
+                // 'category_id' => 'required|exists:categories,id',
+                'categories' => 'required|exists:categories,name',
+                'name' => 'required|string',
+                'description' => 'required|string',
+                'price' => 'required|integer',
+                'picture' => 'required|mimes:jpeg,png,jpg,gif',
+                'condition' => 'required|string',
+            ]);
 
-        if ($product) {
-            Session::flash('status', 'success');
-            Session::flash('message', 'Penambahan data berhasil');
+            $productData = $request->except('picture');
+
+            // Proses upload gambar jika ada
+            if ($request->hasFile('picture')) {
+                $imageName = time() . '.' . $request->picture->getClientOriginalExtension();
+                $request->picture->storeAs('public/uploads/', $imageName);
+                $productData['picture'] = $imageName;
+            }
+
+            $product = Product::create($productData);
+
+            if ($product) {
+                Session::flash('status', 'success');
+                Session::flash('message', 'Penambahan data berhasil');
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            // Menangani kesalahan SQL, misalnya duplikasi data
+            Session::flash('status', 'error');
+            Session::flash('message', 'Penambahan data gagal: ' . $e->getMessage());
+        } catch (\Exception $e) {
+            // Menangani kesalahan umum
+            Session::flash('status', 'error');
+            Session::flash('message', 'Penambahan data gagal: ' . $e->getMessage());
         }
 
         return redirect('/products');
     }
 
-    public function delete($id)
-    {
-        $product = product::findOrFail($id);
-        return view('product.delete', ['product' => $product]);
-    }
-
-    public function destroy($id)
-    {
-        $product = product::findOrFail($id);
-        $product->delete();
-
-        if ($product) {
-            Session::flash('status', 'success');
-            Session::flash('message', 'Penghapusan data berhasil');
-        }
-        return redirect('/products');
-    }
-}
-
+    
